ac3677033f74f68a2a606a43fec704b6f49bf3ea
Ekajto <kac.rutkowski@gmail.com>
2022-04-20 22:44:10 +0200

    change in engine db

--- app/core/db.py
+++ app/core/db.py
@@ -2,9 +2,7 @@ from sqlmodel import Session, SQLModel, create_engine
 
 from app.core.config import settings
 
-args = {"check_same_thread": False} if "sqlite" in settings.DATABASE_URL else None
-
-engine = create_engine(settings.DATABASE_URL, connect_args=args)
+engine = create_engine(settings.DATABASE_URL)
 
 
 def create_db_and_tables():
